95565fba25cdf1b0d4cf5837e7430e6f19263760
V5807 (Course ID) <v5807@cam.ac.uk>
2015-08-14 14:35:45 +0100

    Test drone

--- drone-frontend.js
+++ drone-frontend.js
@@ -1,20 +1,49 @@
 function init() {
-    tracker = initTracker("#example");
+    var tracker = initTracker("#example");
     tracking.track("#example .drone", tracker);
 }
 
 function initTracker(element) {
     // Initialise a color tracker
     var tracker = new tracking.ColorTracker();
-    
+    TrackerUtils.addTrackingColor("#5EA24E", "green", tracker);
+    TrackerUtils.addTrackingColor("#CB7F84", "magenta", tracker);
+    TrackerUtils.addTrackingColor("#A94A45", "red", tracker);
     TrackerUtils.startTrackingColors(tracker);
+
 
     // Whenever there is a new color detected, mark them
     tracker.on('track', function(event) {
         console.log(event.data);
+        markColors(event.data,element);
     });
 
     return tracker;
 }
+function markColors(colors, element) {
+    var canvas = $(element + ' .canvas').get(0);
+    var context = canvas.getContext('2d');
+    context.clearRect(0, 0, context.width, context.height);
+
+    for (var i = 0; i < colors.length; i++) {
+        drawRectangle(colors[i], context);
+        writeRectangle(colors[i], element + ".output");
+    }
+}
+function drawRectangle(rect, context) {
+    context.strokeStyle = rect.color;
+    context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+}
+
+
+// Write the information out in text
+function writeRectangle(rect, element) {
+    $(element)
+        .append("<p>")
+        .append(rect.color + ": " + rect.width + "X" + rect.height)
+        .append(" @ " + rect.x + ":" + rect.y)
+        .append("</p>");
+}
 
 window.addEventListener("load", init);
+
